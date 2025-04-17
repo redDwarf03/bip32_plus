e4d71746d643894fe4fb2fa31817d5f2a4b5ca1d
redDwarf03 <sylvain@archethic.net>
2025-04-17 11:34:59 +0200

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 1.0.0
+- New `bip32_plus` package (because `bip32` package is not maintained. Thanks for their work!!)
+- Update dependencies
+- Update Flutter version
---
+++ lib/bip32_plus.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library bip32_plus;
+
+export 'src/bip32_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
